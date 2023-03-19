b4a4a4062e7309b844395f216eee944a55eef6b7
Mizanur Rahman <mizanur.rahman@bizzntek.com>
2023-03-19 17:27:30 +0600

    Refactor for NID Validation
    Related Work Items: #926

--- Source/gBanker.Web/Controllers/MemberController.cs
+++ Source/gBanker.Web/Controllers/MemberController.cs
@@ -4974,7 +4974,7 @@ namespace gBanker.Web.Controllers
                         entity.NationalID = model.NationalID;
                         if (LoggedInOrganizationID != 150)
                         {
-                            if (entity.NationalID.Length != 17)
+                            if (entity.NationalID.Length != 10 && entity.NationalID.Length != 13 && entity.NationalID.Length != 17)
                             {
                                 return GetErrorMessageResult("NationalID  No cann't be less than 10, 13, 17 digits");
                             }
