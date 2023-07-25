7e50b9217a3283c85a310f3259839a5624fbf5b1
Anika Arabi <v-anika.arabi@grameencommunications.com>
2023-07-25 12:48:45 +0600

    Product Identificaion table with service and repository

---
+++ Source/gBanker.Service/DurationService.cs
@@ -0,0 +1,81 @@
+﻿using gBanker.Data.CodeFirstMigration.InfrastructureBase;
+using gBanker.Data.CodeFirstMigration;
+using gBanker.Data.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gBanker.Service
+{
+    public interface IDurationService : IServiceBase<DurationTable>
+    {
+
+    }
+    public class DurationService : IDurationService
+    {
+        private readonly IDurationRepository repository;
+        private readonly IUnitOfWorkCodeFirst unitOfWork;
+
+        public DurationService(IDurationRepository repository, IUnitOfWorkCodeFirst unitOfWork)
+        {
+            this.repository = repository;
+            this.unitOfWork = unitOfWork;
+        }
+        public IEnumerable<DurationTable> GetAll()
+        {
+            var entities = repository.GetAll();
+            return entities;
+        }
+        public DurationTable GetById(int id)
+        {
+            var entity = repository.GetById(id);
+            return entity;
+        }
+        public DurationTable Create(DurationTable objectToCreate)
+        {
+            repository.Add(objectToCreate);
+            Save();
+            return objectToCreate;
+        }
+        public void Update(DurationTable objectToUpdate)
+        {
+            repository.Update(objectToUpdate);
+            Save();
+        }
+        public void Delete(int id)
+        {
+            var entity = repository.GetById(id);
+            repository.Delete(entity);
+            Save();
+        }
+
+        public void Save()
+        {
+            unitOfWork.Commit();
+        }
+        public bool Inactivate(long id, DateTime? inactiveDate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DurationTable GetByIdLong(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<DurationTable> GetMany(Expression<Func<DurationTable, bool>> where)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsContinued(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+
+    }
+}
---
+++ Source/gBanker.Service/ProductIdentificationService.cs
@@ -0,0 +1,78 @@
+﻿using gBanker.Data.CodeFirstMigration.InfrastructureBase;
+using gBanker.Data.CodeFirstMigration;
+using gBanker.Data.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gBanker.Service
+{
+    public interface IProductIdentificationService : IServiceBase<ProductIdentification>
+    {
+
+    }
+    public class ProductIdentificationService : IProductIdentificationService
+    {
+        private readonly IProductIdentificationRepository repository;
+        private readonly IUnitOfWorkCodeFirst unitOfWork;
+
+        public IEnumerable<ProductIdentification> GetAll()
+        {
+            var entities = repository.GetAll();
+            return entities;
+        }
+
+        public ProductIdentification GetById(int id)
+        {
+            var entity = repository.GetById(id);
+            return entity;
+        }
+
+        public ProductIdentification Create(ProductIdentification objectToCreate)
+        {
+            repository.Add(objectToCreate);
+            Save();
+            return objectToCreate;
+        }
+        public void Update(ProductIdentification objectToUpdate)
+        {
+            repository.Update(objectToUpdate);
+            Save();
+        }
+        public void Delete(int id)
+        {
+            var entity = repository.GetById(id);
+            repository.Delete(entity);
+            Save();
+        }
+        public void Save()
+        {
+            unitOfWork.Commit();
+        }
+
+        public ProductIdentification GetByIdLong(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<ProductIdentification> GetMany(Expression<Func<ProductIdentification, bool>> where)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Inactivate(long id, DateTime? inactiveDate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsContinued(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+
+    }
+}
